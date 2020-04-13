303b872bb30a8f4694be86fcdb5eb11d1a2cf551
Jojo Zhang <jojozhang0327@gmail.com>
2020-04-12 23:32:28 -0400

    add lesson 1 practice

--- src/server.js
+++ src/server.js
@@ -12,16 +12,39 @@ const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
   const rootSchema = `
+    type Cat {
+      name: string
+      age: int
+    }
+    
+    type Query {
+      cat: Cat
+      cats: [cat]!
+    }
+
     schema {
       query: Query
       mutation: Mutation
     }
   `
   const schemaTypes = await Promise.all(types.map(loadTypeSchema))
+  const catResolver = {
+    Query: {
+      cat() {
+        return { name: 'Theo', age: 14 }
+      },
+      cats() {
+        return [
+          { name: 'Theo', age: 14 },
+          { name: 'Dingo', age: 2 }
+        ]
+      }
+    }
+  }
 
   const server = new ApolloServer({
     typeDefs: [rootSchema, ...schemaTypes],
-    resolvers: merge({}, product, coupon, user),
+    resolvers: catResolver,
     async context({ req }) {
       const user = await authenticate(req)
       return { user }
